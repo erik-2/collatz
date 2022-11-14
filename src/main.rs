1e96eccbeae8c190fabb8502ae94ce6added32ac
Eric Tellier <eric.tellier@u-bordeaux.fr>
2022-11-14 17:36:48 +0100

    comparing vanilla/bitwise/reduced bitwise operations

--- src/main.rs
+++ src/main.rs
@@ -31,6 +31,56 @@ fn syracuse(n: BigUint){
     println!("*: {count_multiply} , / {count_divide}");
 }
 
+fn syracuse_bitwise(n: BigUint){
+    let zero: BigUint = Zero::zero();
+    let one: BigUint = One::one();
+    let two: BigUint = 2.to_biguint().unwrap();
+    let mut i: BigUint = n;
+    let mut count_divide = 0;
+    let mut max: BigUint = i.clone();
+    let mut count_multiply = 0;
+    while i != one {
+        if &i % &two == zero {
+            count_divide +=1;
+            i = &i >> 1;
+        }
+        else {
+            count_multiply += 1;
+            i = ((&i <<1) + &i + &one) ;
+        }
+        if &i > &max {
+            max = i.clone();
+        }
+    }
+    let total_iterations = &count_multiply + &count_divide;
+    println!("Max = {max} | Iterations = {total_iterations}");
+    println!("*: {count_multiply} , / {count_divide}");
+}
+
+fn reduced_syracuse_bitwise(n: BigUint){
+    let zero: BigUint = Zero::zero();
+    let one: BigUint = One::one();
+    let two: BigUint = 2.to_biguint().unwrap();
+    let mut i: BigUint = n;
+    let mut count_divide = 0;
+    let mut max: BigUint = i.clone();
+    let mut count_multiply = 0;
+    while i != one {
+        if &i % &two == zero {
+            count_divide +=1;
+            i = &i >> 1;
+        }
+        else {
+            count_multiply += 1;
+            i = ((&i <<1) + &i + &one) >> 1 ;
+        }
+        if &i > &max {
+            max = i.clone();
+        }
+    }
+    let total_iterations = &count_multiply + &count_divide;
+}
+
 
 fn incremental_syracuse(n: &BigUint) -> bool{
     let zero: BigUint = Zero::zero();
@@ -60,26 +110,29 @@ fn incremental_syracuse(n: &BigUint) -> bool{
 }
 
 fn main()-> io::Result<()>  {
-    /*let power = 126;
-    let mut my_big_number: BigUint = i128::pow(2,power).to_biguint().unwrap();
-    let n = 5;
-    println!("n = {n}");
-    let mut size = power;
-    for _ in 1..(n+1) {
-        size += size;
-        my_big_number = my_big_number.clone() * my_big_number;
-    }
-    my_big_number -= 1.to_biguint().unwrap();
-    println!("Length : {}", size);
-    let now = Instant::now();
-    println!("{}",my_big_number);
-    syracuse(my_big_number);
-    println!("Elapsed: {:.2?}", elapsed);
-    */
-    
     let zero: BigUint = Zero::zero();
     let one = 1.to_biguint().unwrap();
     let two = 2.to_biguint().unwrap();
+
+    let power = 10_000;
+    let my_big_number: BigUint = BigUint::pow(&two,power) + &one;
+    let now = Instant::now();
+    println!("{}", &my_big_number);
+    syracuse(my_big_number);
+    println!("Elapsed: {:.2?}", now.elapsed());
+    
+    let my_big_number: BigUint = BigUint::pow(&two,power) + &one;
+    let now = Instant::now();
+    println!("Using bitwise");
+    syracuse_bitwise(my_big_number);
+    println!("Elapsed: {:.2?}", now.elapsed());
+
+    let my_big_number: BigUint = BigUint::pow(&two,power) + &one;
+    let now = Instant::now();
+    println!("Using reduced bitwise : ");
+    reduced_syracuse_bitwise(my_big_number);
+    println!("Elapsed: {:.2?}", now.elapsed());
+    
     let now = Instant::now();
     let power:u32 = 64;
     let mut from: BigUint = BigUint::pow(&two,power);
